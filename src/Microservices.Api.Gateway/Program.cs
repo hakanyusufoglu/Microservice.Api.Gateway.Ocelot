b988fd12b09b8d220aaf9de5cbad49fedc520e75
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-07 09:13:58 +0300

    Ocelot kurulumu tamamlandı

--- src/Microservices.Api.Gateway/Program.cs
+++ src/Microservices.Api.Gateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//ocelot.json DI ekliyoruz
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
+app.UseHttpsRedirection();
 
 app.Run();
--- src/Microservices.Api1/Program.cs
+++ src/Microservices.Api1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! - Api 1");
 
 app.Run();
